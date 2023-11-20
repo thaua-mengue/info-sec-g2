0a226dc714f2c05be5e9eb61ee666656f26116c2
Thauã Mengue <thaua.mengue@gmail.com>
2023-11-20 14:00:43 -0300

    Initial commit

---
+++ src/main/java/br/faccat/infosecg2/InfoSecG2Application.java
@@ -0,0 +1,13 @@
+package br.faccat.infosecg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoSecG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoSecG2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/faccat/infosecg2/InfoSecG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.faccat.infosecg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoSecG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
