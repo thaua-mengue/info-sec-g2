4959e5f152cc42ed30329733bee156e121a08208
Thauã Mengue <thaua.mengue@gmail.com>
2023-11-25 15:52:47 -0300

    add request header authorization

--- src/main/java/br/faccat/infosecg2/controller/CurriculumController.java
+++ src/main/java/br/faccat/infosecg2/controller/CurriculumController.java
@@ -6,7 +6,12 @@ import br.faccat.infosecg2.service.CurriculumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,17 +23,20 @@ public class CurriculumController {
     private final CurriculumService curriculumService;
 
     @GetMapping
-    public ResponseEntity<List<Curriculum>> getAll() {
-        return ResponseEntity.ok(curriculumService.getAll());
+    public ResponseEntity<List<Curriculum>> getAll(@RequestHeader (value = "Authorization") String authorization) {
+        return ResponseEntity.ok(curriculumService.getAll(authorization));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Curriculum> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(curriculumService.getCurriculumById(id));
+    public ResponseEntity<Curriculum> getById(
+            @PathVariable Long id,
+            @RequestHeader (value = "Authorization") String authorization) {
+        return ResponseEntity.ok(curriculumService.getCurriculumById(id, authorization));
     }
 
     @PostMapping
     public ResponseEntity<Curriculum> saveCurriculum(
+            @RequestHeader (value = "Authorization") String authorization,
             @RequestHeader(value = "X-Name") String name,
             @RequestHeader(value = "X-PhoneNumber", required = false) String phoneNumber,
             @RequestHeader(value = "X-Email") String email,
@@ -36,6 +44,6 @@ public class CurriculumController {
             @RequestHeader(value = "X-Experience") String experience) {
 
         var curriculumRequest = new CurriculumRequest(name, phoneNumber, email, webSite, experience);
-        return ResponseEntity.status(HttpStatus.CREATED).body(curriculumService.saveCurriculum(curriculumRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(curriculumService.saveCurriculum(authorization, curriculumRequest));
     }
 }
--- src/main/java/br/faccat/infosecg2/service/CurriculumService.java
+++ src/main/java/br/faccat/infosecg2/service/CurriculumService.java
@@ -4,7 +4,9 @@ import br.faccat.infosecg2.domain.dto.CurriculumRequest;
 import br.faccat.infosecg2.domain.entity.Curriculum;
 import br.faccat.infosecg2.repository.CurriculumRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -12,10 +14,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CurriculumService {
 
+    // O ideal seria essa lista ficar pelo menos em um config-repo para poder ser alterado sem deploy e não ficar exposto no código.
+    private static final List<String> AUTHORIZATIONS_LIST = List.of("Basic aW5mb3NlYzppbmZvc2VjMjAyMzI=");
+
     private final CurriculumRepository curriculumRepository;
 
-    public List<Curriculum> getAll() {
+    public List<Curriculum> getAll(String authorization) {
         try {
+            verifyAuthorization(authorization);
             return curriculumRepository.findAll();
         } catch (RuntimeException ex) {
             System.out.println(ex.getMessage());
@@ -23,8 +29,9 @@ public class CurriculumService {
         }
     }
 
-    public Curriculum getCurriculumById(Long id) {
+    public Curriculum getCurriculumById(Long id, String authorization) {
         try {
+            verifyAuthorization(authorization);
             return curriculumRepository.getReferenceById(id);
         } catch (RuntimeException ex) {
             System.out.println(ex.getMessage());
@@ -32,12 +39,18 @@ public class CurriculumService {
         }
     }
 
-    public Curriculum saveCurriculum(CurriculumRequest curriculumRequest) {
+    public Curriculum saveCurriculum(String authorization, CurriculumRequest curriculumRequest) {
         try {
+            verifyAuthorization(authorization);
             return curriculumRepository.save(new Curriculum(curriculumRequest));
         } catch (RuntimeException ex) {
             System.out.println(ex.getMessage());
             throw ex;
         }
+    }
+
+    private void verifyAuthorization(String authorization) {
+        if (!AUTHORIZATIONS_LIST.contains(authorization))
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Operação não autorizada.");
     }
 }
